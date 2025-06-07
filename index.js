cb4de3548787ef18b4c6a0d08834669e992b33f6
Gideon Mensah <gomy1233@gmail.com>
2025-06-07 22:14:03 +0000

    added eventListener to all buttons

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+/*
 document.querySelector('button').addEventListener('click', handleClick);
 
 function handleClick() {
     alert('Button clicked!');
 }
+*/
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
