186307dc0365dd4e10ab853b58b9c4166a674467
Gideon Mensah <gomy1233@gmail.com>
2025-06-09 12:53:08 +0000

    added pictures to drums

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ document.querySelector('button').addEventListener('click', handleClick);
 function handleClick() {
     alert('Button clicked!');
 }
+
+function changeColor() {
+    document.querySelector('button').style.backgroundColor = 'white';
+}
 */
 
-/*
+
 for (let i = 0; i < document.querySelectorAll("button").length; i++) {
-    document.querySelector('button').addEventListener('click', handleClick);
+
+    document.querySelectorAll('.drum')[i].addEventListener('click', handleClick);
 
     function handleClick() {
-        alert('Button clicked!');
+        // alert('Button clicked!');
+        this.style.color = 'white';
     }
 }
 
-*/
 
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        var audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
-    });
-});
+
+// document.querySelectorAll('.drum').forEach(button => {
+//     button.addEventListener('click', () => {
+//         this.StylePropertyMap.color = 'white';
+//     });
+// });
+
+
+
+
+
+//  var audio = new Audio('./sounds/tom-1.mp3');
+//         audio.play();
