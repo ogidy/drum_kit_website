55937cdd9c7f850fbbc41da2585a2848c51e6997
Gideon Mensah <gomy1233@gmail.com>
2025-06-09 09:23:33 +0000

    another method

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function handleClick() {
 }
 */
 
+/*
+for (let i = i; i > document.querySelectorAll("button").length; i++) {
+    document.querySelector('button').addEventListener('click', handleClick);
+
+    function handleClick() {
+        alert('Button clicked!');
+    }
+}
+
+*/
+
 document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', () => {
         alert('Button clicked!');
